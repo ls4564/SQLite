bd7fa4780461b4eb5460a8ace3deb73c441ea505
Lior1254 <liorshemtov2008@gmail.com>
2025-04-30 17:53:55 +0300

    add AddOrder page

--- app/src/main/java/com/example/sqlite/SignUpEmployeeActivity.java
+++ app/src/main/java/com/example/sqlite/SignUpEmployeeActivity.java
@@ -177,7 +177,8 @@ public class SignUpEmployeeActivity extends AppCompatActivity {
             Intent si = new Intent(this, AddMealActivity.class);
             startActivity(si);
         }else if (id == R.id.p_order) {
-
+            Intent si = new Intent(this, AddOrderActivity.class);
+            startActivity(si);
         }
         return true;
     }
